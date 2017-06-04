bc351ae27ae68d04d974b7b6e75967a5b01b9f3a
David Prentiss <dprentiss@gmail.com>
2017-06-04 17:42:47 -0400

    moved out solarized swatches

---
+++ www/js/caddis.js
@@ -0,0 +1,11 @@
+function webGLStart() {
+    var canvas = document.getElementById("test-canvas");
+    initWebGL(canvas);
+    initShaders();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    drawScene();
+}
